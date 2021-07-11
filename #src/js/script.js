89b2e1d89c3a4b88662a9aea37b38808318c4561
sharko <pavel.sharko@mail.ru>
2021-07-12 02:00:01 +0300

    first commit

---
+++ #src/js/script.js
@@ -0,0 +1,25 @@
+
+new Swiper('.image-slider', {
+    // стрелки
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    // навигация
+    // буллеты, текущее положение, прогрессбар
+    pagination: {
+        el: '.swiper-pagination',
+        // буллеты
+        clickable: true,
+        dinamicbullets: true,
+    },
+    autoHeight: true,
+    loop:true,
+//    автопрокрутка
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+    },
+        speed: 1000,
+    
+})
